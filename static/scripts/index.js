c108acda09d5694b695f8008269e07ed3383e00e
jrmann100 <jordan@mann.com>
2017-08-02 11:34:27 -0700

    Little Changes

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,11 +1,12 @@
-window.onload = () => {
-    $("#hiddentable").show()
-    $("#logo").css('visibility', 'visible').hide().fadeIn('slow');
-    $("h3").delay(1000).hide().delay(800).slideDown(600).fadeIn(300);
-    $("h1").delay(1000).hide().animate({
-        width: 'toggle'
-    }, 500);
-    $("body").show()
-    $("h4").delay(2000).fadeTo(300, 1)
-    $("#otherhiddentable").delay(2000).fadeTo(500, 1)
-};
+window.onload = () => { //Uncomment out of JSFiddle
+$("#hiddentable").show()
+$("#logo").css('visibility', 'visible').hide().fadeIn('slow');
+$("h3").delay(1000).hide().delay(800).slideDown(600).fadeIn(300);
+$("h1").delay(1000).hide().animate({
+  width: 'toggle'
+}, 500);
+$("body").show()
+$("h4").delay(2000).fadeTo(300, 1)
+$("#otherhiddentable").delay(2000).fadeTo(500, 1)
+}; //Uncomment out of JSFiddle
+
